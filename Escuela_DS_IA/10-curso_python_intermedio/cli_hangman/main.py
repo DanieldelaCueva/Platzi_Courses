d0b161cd9a8f5b88ecdf56d293c6600aaef5686b
DanieldelaCueva <danielcuevapuertas@gmail.com>
2022-08-07 22:02:14 +0200

    finished hangman project

--- Escuela_DS_IA/10-curso_python_intermedio/cli_hangman/main.py
+++ Escuela_DS_IA/10-curso_python_intermedio/cli_hangman/main.py
@@ -1,6 +1,8 @@
 import random
 import os
 
+from graphics import HANGMAN_STATES
+
 def setup():
     words = []
     with open("./files/data.txt", 'r', encoding="utf-8") as f:
@@ -17,14 +19,29 @@ def setup():
 def main():
     game_data = setup()
 
-    while list(game_data["word"]) != game_data["word_prompt"]:
-        os.system("clear")
+    opportunities = 0
+
+    used_chars = []
+
+    while list(game_data["word"]) != game_data["word_prompt"] and opportunities < 7:
+        os.system("cls")
         print("Guess the word!")
         for char in game_data["word_prompt"]:
             print(char, end=" ")
         print("\n")
+
+        print(HANGMAN_STATES[opportunities])
+
+        print("Used letters: ", end=" ")
+        for char in used_chars:
+            print(char, end=" ")
+        print("\n")
         
         new_letter = str(input("Enter a new letter! ")).upper()
+
+        if not new_letter in game_data["word"]:
+            opportunities +=1
+            used_chars.append(new_letter.upper())
 
         char_list=list(game_data["word"])
         acc = 0
@@ -35,12 +52,17 @@ def main():
             char_list = char_list[nl_index+1::]
             acc+=(nl_index+1)
 
-    os.system("clear")
-    for char in game_data["word_prompt"]:
+    os.system("cls")
+    for char in list(game_data["word"].upper()):
         print(char, end=" ")
     print("\n")
 
-    print("You won")
+    print(HANGMAN_STATES[opportunities])
+
+    if opportunities == 7:
+        print("You loose")
+    else:
+        print("You win")
 
 
 
